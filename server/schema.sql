7d52ca29c6cbe550876aa8f95ae6205a5c0b6407
Timothy Loo <TimothyLoo07@gmail.com>
2022-08-10 13:09:16 -1000

    save files for latest updates

--- server/schema.sql
+++ server/schema.sql
@@ -1,17 +1,71 @@
-CREATE DATABASE chat;
+-- CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
-);
+/* Describe your table here.*/
 
 /* Create other tables and define schemas for them here! */
-
-
-
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
 
+-- ---
+-- Globals
+-- ---
+
+-- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+-- SET FOREIGN_KEY_CHECKS=0;
+
+-- ---
+-- Table 'messages'
+--
+-- ---
+
+DROP TABLE IF EXISTS `messages`;
+
+CREATE TABLE `messages` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(16) NOT NULL DEFAULT 'NULL',
+  `text` VARCHAR(255) NULL DEFAULT NULL,
+  `timestamp` TIMESTAMP NOT NULL,
+  `id_users` INTEGER NOT NULL,
+  PRIMARY KEY (`id`)
+/*,KEY ()*/
+);
+
+-- ---
+-- Table 'users'
+--
+-- ---
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `email` VARCHAR(30) NOT NULL,
+  `password` VARCHAR(16) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Foreign Keys
+-- ---
+
+ALTER TABLE `messages` ADD FOREIGN KEY (id_users) REFERENCES `users` (`id`);
+
+-- ---
+-- Table Properties
+-- ---
+
+-- ALTER TABLE `messages` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `users` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ---
+-- Test Data
+-- ---
+
+-- INSERT INTO `messages` (`id`,`username`,`text`,`timestamp`,`id_users`) VALUES
+-- ('','','','','');
+-- INSERT INTO `users` (`id`,`email`,`password`) VALUES
+-- ('','','');
